207bd4524566cb4bfb5a5b397aa74b2fd6753c13
MOFAFA <48254156+mofafacodes@users.noreply.github.com>
2023-05-16 04:15:45 +0000

    Added health check endpoints.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Text.Json;
+using System.Net.Mime;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,31 +19,103 @@ builder.Services.AddControllers(options =>
 //Mongo Db registration
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+//pulling out mongodb settings to reduce them later on
+var mongoDBSettings = builder.Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
+
 
 builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
 {
-    var settings = builder.Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
-    return new MongoClient(connectionString: settings?.ConnectionString);
+//mongo db settings used here as mongodb client singleton
+    return new MongoClient(connectionString: mongoDBSettings?.ConnectionString);
 }
 );
 builder.Services.AddSingleton<IItemRepository, MongoDBItemsRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
+}); 
+builder.Services.AddHealthChecks()
+    //using mongodb settings to specify the connection string that should be used to connect to the databse
+    //hence health check will be based on is database reachable or not
+    //add a timeout period of 3 seconds after which health check will be considered failed if not able to connect to database
+    .AddMongoDb(
+    mongodbConnectionString: mongoDBSettings.ConnectionString, 
+    name: "mongodb", 
+    timeout: TimeSpan.FromSeconds(3),
+    //adding tags to help group health checks
+    tags: new[] { "ready" }
+    );
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{ 
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseRouting();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    //ready will make sure out api is ready to receive requests
+    endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+    {
+        //specify predicate to filter out which health checks you want to include in endpoint
+        Predicate = (check) => check.Tags.Contains("ready"),
+        //customizing message to recieve when making helath checks
+        ResponseWriter = async(context, report) =>
+        {
+            var result = JsonSerializer.Serialize(
+                new
+                {
+                    status = report.Status.ToString(),
+                    checks = report.Entries.Select(entry => new
+                    {
+                        name = entry.Key,
+                        status = entry.Value.Status.ToString(),
+                        execption = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                        duration = entry.Value.Duration.ToString()
+                    })
+                });
+            //format output
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+        }
+    });
+    //ready will make sure out api is alive or up and running
+    endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+    {
+        //specify predicate to filter out which health checks you want to include in endpoint
+        Predicate = (check) => check.Tags.Contains("live"),
+        ResponseWriter = async (context, report) =>
+         {
+             var result = JsonSerializer.Serialize(
+                 new
+                 {
+                     status = report.Status.ToString(),
+                     checks = report.Entries.Select(entry => new
+                     {
+                         name = entry.Key,
+                         status = entry.Value.Status.ToString(),
+                         execption = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                         duration = entry.Value.Duration.ToString()
+                     })
+                 });
+             //format output
+             context.Response.ContentType = MediaTypeNames.Application.Json;
+             await context.Response.WriteAsync(result);
+         }
+    });
+});
 
 app.Run();
+ 
--- Settings/MongoDBSettings.cs
+++ Settings/MongoDBSettings.cs
@@ -3,12 +3,15 @@
     public class MongoDBSettings
     {
         public string? Host { get; set; }
-        public  int Port { get; set; }
+        public int Port { get; set; }
+        public string? User { get; set; }
+        public string? Password { get; set; }
         public string ConnectionString
         {
             get
             {
-                return $"mongodb://{Host}:{Port}";            }
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
+            }
         }
     }
 }
